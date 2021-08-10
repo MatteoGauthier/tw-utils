556e18a2a4d92a9e3aa4bc5ed7305723dcef4be0
Mattèo Gauthier <matteo.gauthier@gmail.com>
2021-08-10 10:31:30 +0200

    remove extend

--- font/inter/sans-fallback.js
+++ font/inter/sans-fallback.js
@@ -2,10 +2,17 @@ const defaultTheme = require("tailwindcss/defaultTheme")
 const fallbackMacOsFonts = require("../../lib/fallback-macos-fonts")
 module.exports = {
 	theme: {
-		extend: {
-			fontFamily: {
-				display: ["ui-sans-serif", "system-ui", "-apple-system", "BlinkMacSystemFont", '"Helvetica Neue"', "'Inter'", "Inter", ...defaultTheme.fontFamily.sans],
-			},
+		fontFamily: {
+			display: [
+				"ui-sans-serif",
+				"system-ui",
+				"-apple-system",
+				"BlinkMacSystemFont",
+				'"Helvetica Neue"',
+				"'Inter'",
+				"Inter",
+				...defaultTheme.fontFamily.sans,
+			],
 		},
 	},
 }
